0c89273277135caa03b842ab3229723a7bf7ada2
Dhananjay Chavan <219833325+cdhananjay@users.noreply.github.com>
2026-03-01 13:43:13 +0530

    create draw and discard method for cards, create seperate card piles, update readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # (unfinished) uno engine
+
+### Card Colours:
+Red, Green, Blue, Yellow
+
+### Total Cards:
+- Numbers : two copies of each 1 to 9 for each colour, one 0 for each colour
+- Skip : two for each colour
+- Reverse: two for each colour
+- Draw2 : two uncoloured
+- Draw4 : two uncoloured
+
+### Initialization:
+- Shuffle the deck.
+- Deal 7 cards to each player.
+- Place the remaining cards face down → Draw pile.
+- Flip the top card face up → starts the Discard pile.
+
+### Valid Moves:
+| card to discard | top card on discard pile | valid condition      |
+|-----------------|--------------------------|----------------------|
+| number          | number                   | same value or colour |
+| number          | skip                     | same colour          |
+| skip            | number                   | same colour          |
+| skip            | skip                     | always               |
+| skip            | reverse                  | same colour          |
+| reverse         | number                   | same colour          |
+| reverse         | skip                     | same colour          |
+| reverse         | reverse                  | always               |
+| anything        | draw                     | always               |
+| draw            | anything                 | always               |
+
+### Card Piles:
+- Discard Pile : You play cards onto the discard pile (face up).
+- Draw Pile : You draw from the draw pile (face down). When the draw pile runs out, shuffle the discard pile (except the top card). That becomes the new draw pile.
--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 interface IGame {
+    reversed: boolean
     players : IPlayer[]
     deck : ICard[]
+    drawPile: ICard[]
+    discardPile: ICard[]
+    currTurn : number
+}
+
+interface IPlayer {
+    cards: ICard[]
+    id : number
+    isBot : boolean
 }
 
 interface ICard {
     type : CardTypes
     colour ?: Colours
     value ?: number
-}
-
-interface IPlayer {
-    id : number,
-    isBot: boolean,
-    cards: ICard[],
-    addCard: (cards: ICard) => void
-    dropCard: (card : ICard) => void
 }
 
 enum Colours {
@@ -26,31 +28,148 @@ enum CardTypes {
 }
 
 class Game implements IGame{
+    reversed: boolean = false
     players : IPlayer[]
-    deck : ICard[]
+    deck : ICard[] = []
+    drawPile: ICard[] = []
+    discardPile: ICard[] = []
+    currTurn = 0
     constructor(players: IPlayer[]) {
         this.players = players;
-        this.deck = [];
+        this.fillDeck();
+        this.shuffleCards(this.deck);
+        this.distributeCards();
     }
-    addToDeck(card: ICard) {
-        this.deck.push(card)
-    }
-    start(){
-        // fill deck
-        for (let value = 0; value < 10; value++) {
-            for (let colour = 0; colour < 4; colour++) {
-                this.addToDeck(new Card(CardTypes.NUMBER, colour, value))
+    fillDeck(){
+        // NUMBER : two copies of each 1 to 9 for each colour
+        for (let i = 0; i < 2; i++) {
+            for (let value = 1; value < 10; value++) {
+                for (let colour = 0; colour < 4; colour++) {
+                    this.deck.push(new Card(CardTypes.NUMBER, colour, value))
+                }
             }
         }
-        for (let i = 0; i < 2; i++) {
-            this.addToDeck(new Card(CardTypes.DRAW4))
-            this.addToDeck(new Card(CardTypes.DRAW2))
-        }
-
+        // NUMBER : one 0 for each colour
         for (let colour = 0; colour < 4; colour++) {
-            this.addToDeck(new Card(CardTypes.REVERSE, colour))
-            this.addToDeck(new Card(CardTypes.SKIP, colour))
+            this.deck.push(new Card(CardTypes.NUMBER, colour, 0))
         }
+        // REVERSE and SKIP : two for each colour
+        for (let colour = 0; colour < 4; colour++) {
+            this.deck.push(new Card(CardTypes.REVERSE, colour))
+            this.deck.push(new Card(CardTypes.SKIP, colour))
+        }
+        // DRAW2 and DRAW4 : two uncoloured
+        for (let i = 0; i < 2; i++) {
+            this.deck.push(new Card(CardTypes.DRAW4))
+            this.deck.push(new Card(CardTypes.DRAW2))
+        }
+    }
+    shuffleCards(cards : ICard[]) {
+        for (let i = 0; i < cards.length; i++) {
+            const j = Math.floor(Math.random()*cards.length)
+            const k = Math.floor(Math.random()*cards.length)
+            const temp = cards[j]
+            cards[j] = cards[k]
+            cards[k] = temp
+        }
+    }
+    distributeCards() {
+        const initialCardsPerPlayer = 7
+        for (let j = 0; j < initialCardsPerPlayer; j++) {
+            for (let i = 0; i < this.players.length; i++) {
+                this.drawCard(this.players[i])
+            }
+        }
+        for (let i = 0; i < this.deck.length - 1; i++) {
+            this.drawPile.push(this.deck.pop());
+        }
+        this.discardPile.push(this.deck.pop());
+    }
+
+    updateCurrTurn() {
+        if (this.reversed) {
+            if (this.currTurn > 0) this.currTurn--;
+            else this.currTurn = this.players.length - 1;
+        }
+        else{
+            if (this.currTurn < this.players.length - 1) this.currTurn++;
+            else this.currTurn = 0;
+        }
+    }
+    playTurn(card : ICard) {
+        const topDiscardPileCard = this.discardPile[-1]
+        if (topDiscardPileCard.type === CardTypes.DRAW2) for (let i = 0; i < 2; i++) this.drawCard(this.players[this.currTurn])
+        else if (topDiscardPileCard.type === CardTypes.DRAW4) for (let i = 0; i < 4; i++) this.drawCard(this.players[this.currTurn])
+        const playableCards = this.getPlayableCards();
+        if (playableCards.length === 0) {
+            this.drawCard(this.players[this.currTurn])
+            this.updateCurrTurn()
+            return true
+        }
+        if (playableCards.indexOf(card) != -1) {
+            this.discardCard(this.players[this.currTurn], this.players[this.currTurn].cards.indexOf(card))
+            if (card.type === CardTypes.REVERSE) this.reversed = !this.reversed;
+            else if (card.type === CardTypes.SKIP) this.updateCurrTurn();
+            this.updateCurrTurn();
+            return true
+        }
+        return false
+    }
+
+    getPlayableCards() {
+        return this.players[this.currTurn].cards.filter(card => this.isValidDiscard(card))
+    }
+
+    drawCard(player : IPlayer) {
+        if (this.drawPile.length > 0) player.cards.push(this.drawPile.pop())
+        // When the draw pile runs out, shuffle the discard pile (except the top card). That becomes the new draw pile.
+        const topCardOfDiscardPile = this.discardPile.pop()
+        this.shuffleCards(this.discardPile)
+        this.drawPile = this.discardPile
+        this.discardPile = [topCardOfDiscardPile]
+    }
+    discardCard(player: IPlayer, index : number) {
+        if (!this.isValidDiscard(player.cards[index])) return false
+        this.discardPile.push(player.cards[index])
+        player.cards.splice(index, 1)
+        return true
+    }
+    isValidDiscard(card: ICard) {
+        // drop on empty deck
+        if (this.discardPile.length == 0) return true
+        const discardPileTopCard = this.discardPile[-1]
+        // drop anything on draw card
+        if (discardPileTopCard.type === CardTypes.DRAW2 || discardPileTopCard.type === CardTypes.DRAW4 ) return true
+        // drop draw card on anything
+        if (card.type === CardTypes.DRAW2 || card.type === CardTypes.DRAW4 ) return true
+        // drop any colour reverse on any colour reverse
+        if (card.type === CardTypes.REVERSE && discardPileTopCard.type === CardTypes.REVERSE) return true
+        // drop any colour skip on any colour skip
+        if (card.type === CardTypes.SKIP && discardPileTopCard.type === CardTypes.SKIP) return true
+        // drop same colour reverse on number/skip
+        if (card.type === CardTypes.REVERSE && (discardPileTopCard.type === CardTypes.NUMBER || discardPileTopCard.type === CardTypes.SKIP ) && card.colour === discardPileTopCard.colour) return true
+        // drop same colour skip on number/reverse
+        if (card.type === CardTypes.SKIP && (discardPileTopCard.type === CardTypes.NUMBER || discardPileTopCard.type === CardTypes.REVERSE ) && card.colour === discardPileTopCard.colour) return true
+        // drop same colour number on reverse/skip
+        if (card.type === CardTypes.NUMBER && (discardPileTopCard.type === CardTypes.REVERSE || discardPileTopCard.type === CardTypes.SKIP ) && card.colour === discardPileTopCard.colour) return true
+        // drop same colour or value number on number
+        if (card.type === CardTypes.NUMBER && discardPileTopCard.type === CardTypes.NUMBER  && (card.colour === discardPileTopCard.colour || card.value === discardPileTopCard.value)) return true
+
+        // everything else is not valid
+        return false
+    }
+}
+
+class Player implements IPlayer {
+    cards: ICard[]
+    id : number
+    isBot : boolean = false
+    constructor(id: number, isBot ?: boolean) {
+        this.id = id;
+        if (isBot != undefined) this.isBot = isBot;
+    }
+    hasWon() {
+        return this.cards.length === 0;
     }
 }
 
@@ -60,25 +179,7 @@ class Card implements ICard {
     value ?: number
     constructor(type : CardTypes, colour ?: Colours, value ?: number ) {
         this.type = type
-        if (this.type === CardTypes.NUMBER) this.value = value
         if (this.type === CardTypes.NUMBER || this.type === CardTypes.REVERSE || this.type === CardTypes.SKIP) this.colour = colour
-    }
-}
-
-class Player implements IPlayer {
-    cards: ICard[]
-    id : number
-    isBot : boolean
-    constructor(id: number, isBot : boolean) {
-        this.id = id;
-        this.isBot = isBot;
-    }
-    addCard(card : ICard) {
-        this.cards.push(card)
-    }
-    dropCard(card : ICard) {
-        let index = this.cards.indexOf(card)
-        if (index === -1) throw Error(`player ${this.id} does not have ${card}`)
-        this.cards.splice(index,1);
+        if (this.type === CardTypes.NUMBER) this.value = value
     }
 }
